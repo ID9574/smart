d235f46e14fa8c3874c7c2c82baea387ebb93396
杨鹏 <roc9574@sina.com>
2016-10-11 13:25:04 +0800

    第一个简单的JavaWeb应用

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # smart
 架构探险 从零开始写Java Web 框架 书本 实操案例
+
+第一天
+chapter_01 「从一个简单的Web应用开始」
+    使用IDEA与Maven创建项目
+    正确配置Maven配置文件及其配置项含义理解
+    搭建Web项目框架
+    在Maven配置中添加JavaWeb的依赖
+    编写一个简单Servlet类
+    编写一个简单的Jsp页面
+    在IDEA中配置Tomcat
+    提交代码至Git仓库及其远程推送
+
---
+++ chapter_01/src/main/java/studio/airwolf/chapter_01/HelloServlet.java
@@ -0,0 +1,31 @@
+package studio.airwolf.chapter_01;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author master
+ * @Title: smart
+ * @Package studio.airwolf.chapter_01
+ * @Description:(文件描述)
+ * @Created 2016-10-11 11:56
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/WEB-INF/views/hello.jsp").forward(req,resp);
+    }
+
+}
